760a2b28cc1296655817ec4be90fa649453ee2e9
max <maxkr1203@gmail.com>
2023-01-14 15:33:05 +0200

    add dependencies to client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,36 @@
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../store/store";
+import { DehydratedState, QueryClient } from "@tanstack/query-core";
+import { useState } from "react";
+import { Hydrate, QueryClientProvider } from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+export default function App({
+	Component,
+	pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false,
+					},
+				},
+			})
+	);
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Hydrate state={pageProps.dehydratedState}>
+				<Provider store={store}>
+					<ChakraProvider>
+						<Component {...pageProps} />
+					</ChakraProvider>
+				</Provider>
+			</Hydrate>
+		</QueryClientProvider>
+	);
 }
