cbdd9f247aeddeda9833a277978c70fc0deb006e
max <maxkr1203@gmail.com>
2023-01-26 23:22:51 +0200

    perf: add user slice

---
+++ client/src/store/reducers/user.slice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AuthResponse } from "../../types/Auth";
+import { IUser } from "../../types/IUser";
+
+interface InitialState extends AuthResponse {
+	user: IUser;
+	isAuth: boolean;
+}
+
+const initialState: InitialState = {} as InitialState;
+
+const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		setUser: (state, action: PayloadAction<AuthResponse>) => {
+			localStorage.setItem("token", action.payload.accessToken);
+
+			return { ...state, ...action.payload, isAuth: true };
+		},
+		removeUser: () => {
+			localStorage.removeItem("token");
+
+			return { ...({} as InitialState), isAuth: false };
+		},
+	},
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./reducers/user.slice";
 
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		user: userSlice,
+	},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
