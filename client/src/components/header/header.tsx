6e07b3f6bdad4c2fc49c05df1fe77709b02fb07e
max <maxkr1203@gmail.com>
2023-01-28 23:31:53 +0200

    perf: add header component

---
+++ client/src/components/header/header.tsx
@@ -0,0 +1,83 @@
+import React, { FC } from "react";
+import {
+	Box,
+	Button,
+	Heading,
+	Popover,
+	PopoverArrow,
+	PopoverBody,
+	PopoverCloseButton,
+	PopoverContent,
+	PopoverHeader,
+	PopoverTrigger,
+	useToast,
+} from "@chakra-ui/react";
+import { FaUserAlt } from "react-icons/fa";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { useMutation } from "@tanstack/react-query";
+import { userService } from "../../services/user.service";
+import { removeUser } from "../../store/reducers/user.slice";
+import { useRouter } from "next/router";
+import ColorModeSwitcher from "../colorModeSwitcher";
+import Link from "next/link";
+
+const Header: FC = () => {
+	const router = useRouter();
+	const dispatch = useAppDispatch();
+	const user = useAppSelector((state) => state.user);
+	const toast = useToast();
+
+	const logOut = useMutation({
+		mutationKey: ["logout"],
+		mutationFn: () => userService.logout(),
+		onSuccess: () => {
+			dispatch(removeUser());
+			router.push("/");
+			toast({
+				title: "Logged out successfully",
+				status: "success",
+				isClosable: true,
+				duration: 5000,
+			});
+		},
+	});
+
+	return (
+		<Box
+			as="header"
+			py="4"
+			display="flex"
+			alignItems="center"
+			justifyContent="space-between"
+			borderBottom="2px"
+		>
+			<Heading as="h1" size="lg">
+				<Link href="/">Rnotes</Link>
+			</Heading>
+
+			<Popover>
+				<PopoverTrigger>
+					<Button borderRadius="50%" p="0">
+						<FaUserAlt />
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent>
+					<PopoverArrow />
+					<PopoverCloseButton />
+					<PopoverHeader>{user.user?.email}</PopoverHeader>
+					<PopoverBody display="flex" justifyContent="space-between">
+						<Button
+							aria-label="user info and logout button"
+							onClick={() => logOut.mutate()}
+						>
+							Logout
+						</Button>
+						<ColorModeSwitcher />
+					</PopoverBody>
+				</PopoverContent>
+			</Popover>
+		</Box>
+	);
+};
+
+export default Header;
