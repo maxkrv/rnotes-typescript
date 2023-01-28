eeff965f1f89ba5f1347908e7447e7160c316560
max <maxkr1203@gmail.com>
2023-01-28 23:26:59 +0200

    fix: change request function

--- client/src/providers/authProvider.tsx
+++ client/src/providers/authProvider.tsx
@@ -1,17 +1,17 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useEffect } from "react";
 import axios from "axios";
 import { useAppDispatch } from "../hooks/redux";
 import { AuthResponse } from "../types/Auth";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { setUser } from "../store/reducers/user.slice";
 import { Box, Spinner } from "@chakra-ui/react";
 
 const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
 	const dispatch = useAppDispatch();
 
-	const { isLoading } = useQuery({
-		queryKey: ["refresh"],
-		queryFn: () =>
+	const { mutate, isLoading } = useMutation({
+		mutationKey: ["refresh"],
+		mutationFn: () =>
 			axios.get<AuthResponse>(
 				`${process.env.NEXT_PUBLIC_SERVER_URL}/api/refresh`,
 				{
@@ -21,9 +21,13 @@ const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
 		onSuccess: (data) => {
 			dispatch(setUser(data.data));
 		},
-		enabled: typeof window !== "undefined" && !!localStorage.getItem("token"),
-		retry: false,
 	});
+
+	useEffect(() => {
+		if (typeof window !== "undefined" && !!localStorage.getItem("token")) {
+			mutate();
+		}
+	}, [mutate]);
 
 	return (
 		<>
