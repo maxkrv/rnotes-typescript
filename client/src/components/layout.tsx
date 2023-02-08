6296c658c60bcb4d063085c7c883f54ff68ec4f6
max <maxkr1203@gmail.com>
2023-02-08 23:24:03 +0200

    perf: change logic on layout

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,13 +1,68 @@
-import React, { ComponentPropsWithoutRef, FC } from "react";
-import { Container } from "@chakra-ui/react";
+import React, {
+	ComponentPropsWithoutRef,
+	FC,
+	useEffect,
+	useState,
+} from "react";
+import { Container, useColorModeValue } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import Header from "./header/header";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/tabs";
+import Link from "next/link";
 
 const Layout: FC<ComponentPropsWithoutRef<"div">> = ({
 	children,
 	...props
 }) => {
+	const { pathname } = useRouter();
+	const bg = useColorModeValue("gray.300", "gray.700");
+	const [tabIndex, setTabIndex] = useState(0);
+
+	useEffect(() => {
+		if (pathname === "/notes" || pathname === "/notes/[noteId]") {
+			setTabIndex(0);
+		} else if (pathname === "/todos" || pathname === "/todos/[todoId]") {
+			setTabIndex(1);
+		}
+	}, [pathname]);
+
 	return (
-		<Container height="100%" maxW="6xl" {...props}>
-			{children}
+		<Container
+			display="flex"
+			flexDirection="column"
+			flex="1"
+			height="100%"
+			maxW="6xl"
+			{...props}
+		>
+			{pathname !== "/" && <Header />}
+			{pathname === "/" ? (
+				children
+			) : (
+				<Tabs
+					display="flex"
+					flexDirection="column"
+					flex="1"
+					my="6"
+					isFitted
+					variant="solid-rounded"
+					index={tabIndex}
+					onChange={(index) => setTabIndex(index)}
+				>
+					<TabList>
+						<Tab borderRadius="0px" as={Link} href="/notes">
+							Notes
+						</Tab>
+						<Tab borderRadius="0px" as={Link} href="/todos">
+							To-dos
+						</Tab>
+					</TabList>
+					<TabPanels bg={bg} flex="1">
+						<TabPanel tabIndex={0}>{children}</TabPanel>
+						<TabPanel tabIndex={1}>{children}</TabPanel>
+					</TabPanels>
+				</Tabs>
+			)}
 		</Container>
 	);
 };
