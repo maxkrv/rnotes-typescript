de91fbd85ee6405809016a3080f5945d250438cf
maxkrv <maxkr1203@gmail.com>
2023-03-19 23:22:08 +0200

    perf: change seo config

--- client/seo.config.js
+++ client/seo.config.js
@@ -7,11 +7,20 @@ export default {
 		type: "website",
 		locale: "en_IE",
 		url: `${process.env.NEXT_PUBLIC_CLIENT_URL}`,
+		site_name: "Rnotes",
 		title: "Rnotes",
 		description: "Free, safe to-dos and notes.",
+		images: [
+			{
+				url: `${process.env.NEXT_PUBLIC_CLIENT_URL}/favicon/favicon-32x32.png`,
+				width: 32,
+				height: 32,
+				alt: "Rnotes",
+			},
+		],
 	},
 	twitter: {
-		handle: "@handle",
+		handle: "@leenp1ck",
 		site: "@rnotes",
 		cardType: "summary_large_image",
 	},
